419f06068504ce88cc35c894aca9b94ac79adf10
jianm <jianma321@gmail.com>
2019-05-24 10:40:41 -0500

    second update

---
+++ app/src/main/java/com/example/insapp/MainActivity.java
@@ -0,0 +1,83 @@
+package com.example.insapp;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+import com.example.insapp.Fragment.HomeFragment;
+import com.example.insapp.Fragment.NotificationFragment;
+import com.example.insapp.Fragment.ProfileFragment;
+import com.example.insapp.Fragment.SearchFragment;
+import com.google.firebase.auth.FirebaseAuth;
+import io.reactivex.annotations.NonNull;
+
+public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+    Fragment selectedfragment = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
+
+        Bundle intent = getIntent().getExtras();
+        if(intent != null){
+            String publisher = intent.getString("publisherid");
+            SharedPreferences.Editor editor = getSharedPreferences("PREFS", MODE_PRIVATE).edit();
+            editor.putString("profileid", publisher);
+            editor.apply();
+
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new ProfileFragment()).commit();
+        } else{
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new HomeFragment()).commit();
+        }
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                new HomeFragment()).commit();
+    }
+
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                    switch (item.getItemId()){
+                        case R.id.nav_home:
+                            selectedfragment = new HomeFragment();
+                            break;
+                        case R.id.nav_search:
+                            selectedfragment = new SearchFragment();
+                            break;
+                        case R.id.nav_add:
+                            selectedfragment = null;
+                            startActivity(new Intent(MainActivity.this, PostActivity.class));
+                            break;
+                        case R.id.nav_heart:
+                            selectedfragment = new NotificationFragment();
+                            break;
+                        case R.id.nav_profile:
+                            SharedPreferences.Editor editor = getSharedPreferences("PREFS", MODE_PRIVATE).edit();
+                            editor.putString("profileid", FirebaseAuth.getInstance().getCurrentUser().getUid());
+                            editor.apply();
+                            selectedfragment = new ProfileFragment();
+                            break;
+                    }
+                    if (selectedfragment != null) {
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                                selectedfragment).commit();
+                    }
+
+                    return true;
+                }
+            };
+}
